e03557ccb8b1d0295f4537f67313fe5a656c918d
sterlu <sterlu.sd@gmail.com>
2021-01-11 18:07:45 +0100

    Add ESM build

---
+++ webpack.umd.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+  mode: "none",
+  entry: './src/index.js',
+  output: {
+    library: 'defisaver-tokens',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'umd'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  "targets": {
+                    "esmodules": true
+                  }
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  externals: ["react", "decimal.js"],
+};
