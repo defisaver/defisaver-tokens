4507343acd019b9ce7a229a219bcfef4e3f80017
Cussone <aleksandar.cakalic@gmail.com>
2021-01-21 18:04:38 +0100

    Add uuid to asset's icon id and fill attributes

--- src/TokenIcons/BlankIcon.js
+++ src/TokenIcons/BlankIcon.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 
 export default function BlankIcon(props) {
+  const id = `blank_icon_${uuid()}`;
+  const fill = `url(#${id})`;
   return (
     <svg width="193" height="193" viewBox="0 0 193 193" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <circle cx="96.5" cy="96.5" r="96.5" transform="rotate(-180 96.5 96.5)" fill="url(#paint0_linear)" fillOpacity="0.9" />
+      <circle cx="96.5" cy="96.5" r="96.5" transform="rotate(-180 96.5 96.5)" fill={fill} fillOpacity="0.9" />
       <defs>
-        <linearGradient id="paint0_linear" x1="193" y1="2.46321" x2="193" y2="193" gradientUnits="userSpaceOnUse">
+        <linearGradient id={id} x1="193" y1="2.46321" x2="193" y2="193" gradientUnits="userSpaceOnUse">
           <stop stopColor="#182025"/>
           <stop offset="1" stopColor="#2A5648" />
         </linearGradient>
