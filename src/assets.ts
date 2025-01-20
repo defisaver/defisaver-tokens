b4c8e08489fd41d5c26f63102120deaa1025d9bf
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2025-01-20 14:02:50 +0100

    Update BOLD address

--- src/assets.ts
+++ src/assets.ts
@@ -230,7 +230,7 @@ const SKYAddress = "0x56072C95FAA701256059aa122697B133aDEd9279";
 const sUSDSAddress = "0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD";
 const rswETHAddress = "0xFAe103DC9cf190eD75350761e95403b7b8aFa6c0";
 const wsuperOETHbAddress = "0x7FcD174E80f264448ebeE8c88a7C4476AAF58Ea6";
-const boldAddress = "0x28514d2e99add34af36dd779126195fe053e4f97";
+const boldAddress = "0xcd998e9e6ef0d4117dd0c2d9eae0b12e81f92d00";
 
 
 export const assetProto: AssetDataBase = {
