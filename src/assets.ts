d76a98c334ed8b46048e01048a8d3de1c9059fef
stefanmiric <miricstefann@gmail.com>
2025-08-28 21:14:48 +0200

    Add tBTC address for Arbitrum and version bump to 1.6.23

--- src/assets.ts
+++ src/assets.ts
@@ -2815,6 +2815,7 @@ export const assets: AssetDataBase[] = [
     addresses: {
       1: tBTCAddress,
       8453: '0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b',
+      42161: '0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40',
     },
     decimals: 18,
   },
